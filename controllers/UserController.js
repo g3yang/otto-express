e4b82ef6f3a442106430d4df9fd6ad5302ed5e43
Nicolas Yang <=guangwei85@gmail.com>
2017-10-11 23:27:35 -0400

    Added signup method

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('../middlewares/Passport');
+var users = require('../models/Users');
+var _ = require('lodash');
 
 router.get('/user', passport.login(), (req,res)=>{
-    return res.json(req.user);
+    res.json(req.user);
 });
+
+router.post('/users', (req, res)=>{
+    let {email, password, id} = req.body;
+
+    let user = _.find(users, {email:email});
+    if(user){
+        return res.send(400, {
+            error_message: 'This email has been used.'
+        });
+    }
+    let newUser = {
+        email,
+        password,
+        id
+    };
+    users.push(newUser);
+    return res.json(newUser);
+});
+
+
+
 
 module.exports = router;
